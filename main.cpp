497c51d4c05e16cca5a3de2071be81c43306baa7
Brandon Lam <lamb@os2.engr.oregonstate.edu>
2021-04-06 18:37:11 -0700

    File I/O working properly

--- main.cpp
+++ main.cpp
@@ -18,6 +18,11 @@ struct Node { //keep track of the parent node, current node and depth.
 //We will create two GameState objects, one called Left and one called Right, where each game object contains the number of items on the river.
 
 //TEMPORARY GAME STRUCTURE
+/* TXT FILE STRUCTURE: 
+*  first line left: numChickens, numWolves, boat
+*  second line right: numChickens, numWolves, boat
+*/
+
 class River{
 	public:
 		int numObjects(); //return total number of objects on a river side
@@ -35,7 +40,7 @@ class River{
 
 int River::numObjects() {
 	int BOATGANG = 0;
-	if (hasBoat == true) {
+	if (hasBoat() == 1) {
 		BOATGANG = 1;
 	}
 	int total = getNumWolves() + getNumChickens() + BOATGANG;
@@ -81,8 +86,11 @@ bool River::hasBoat() {
 
 //Function Declarations
 
+void readFromFiles(River&, River&, River&, River&, std::ifstream&, std::ifstream&);
+
 int main(int argc, char** argv) {
 	if (argc != 5) { //check for correct number of arguments
+		//arguments should be executable initial.txt goal.txt mode output.txt 
 		std::cout << "Invalid number of arguments. argc != 5" << std::endl;
 		return 1;
 	}
@@ -91,8 +99,8 @@ int main(int argc, char** argv) {
 	std::string mode = argv[3];
 	std::string outputFile = argv[4];
 
-	class River leftInitial;
-	class River rightInitial;
+	class River leftBank;
+	class River rightBank;
 	class River leftGoal;
 	class River rightGoal;
 
@@ -109,6 +117,14 @@ int main(int argc, char** argv) {
 	}
 
 	//fill initial and goal states
+	readFromFiles(leftBank, rightBank, leftGoal, rightGoal, initialFIN, goalFIN);
+
+	/*
+	std::cout << "left bank initial: " << leftBank.getNumChickens() << " " << leftBank.getNumWolves() << " " << leftBank.hasBoat() << std::endl;
+	std::cout << "right bank initial: " << rightBank.getNumChickens() << " " << rightBank.getNumWolves() << " " << rightBank.hasBoat() << std::endl;
+	std::cout << "left bank gole: " << leftGoal.getNumChickens() << " " << leftGoal.getNumWolves() << " " << leftGoal.hasBoat() << std::endl;
+	std::cout << "right bank goul: " << rightGoal.getNumChickens() << " " << rightGoal.getNumWolves() << " " << rightGoal.hasBoat() << std::endl;
+	*/
 
 	//close files after we are done with them
 	initialFIN.close();
@@ -133,7 +149,59 @@ int main(int argc, char** argv) {
 	}
 
 	//write solution to output file and print to the terminal, then close the output file
+	//clean up objects
 
 	outputFOUT.close();
 	return 0;
+}
+
+/*******************************************************************************************************************
+* Function Name: readFromFiles
+* Description: Reads shit from the initial and goal.txt files and stores them in our four objects using the infiles. 
+* Parameters: 4 objects and two ifstream variables
+* Output: nothing
+******************************************************************************************************************/
+
+
+void readFromFiles(River& leftBank, River& rightBank, River& leftGoal, River& rightGoal, std::ifstream& initialFIN, std::ifstream& goalFIN) {
+	int chickens = 0;
+	int wolves = 0;
+	bool bote;
+	char temp69420;
+	char temp;
+	//get contents from first line of the initial.txt
+	initialFIN >> chickens >> temp69420 >> wolves >> temp >> bote;
+	//store them into the object (leftBank)
+	leftBank.setNumChickens(chickens);
+	leftBank.setNumWolves(wolves);
+	leftBank.setBoat(bote);
+
+	/*
+	std::cout << "Left Bank Initial " << chickens << " " << wolves << " " << bote << std::endl;
+	std::cout << "Left Bank Initial " << leftBank.getNumChickens() << " " << leftBank.getNumWolves() << " " << leftBank.hasBoat() << std::endl;
+	*/
+
+	//get contents from second line of initial.txt
+	initialFIN >> chickens >> temp >> wolves >> temp69420 >> bote;
+	//store in rightBank object
+	rightBank.setNumChickens(chickens);
+	rightBank.setNumWolves(wolves);
+	rightBank.setBoat(bote);
+	
+	//std::cout << "Right Bank Initial " << chickens << " " << wolves << " " << bote << std::endl;
+
+	//get contents from first line of goal.txt
+	goalFIN >> chickens >> temp >> wolves >> temp69420 >> bote;
+	//store them in leftGoal object
+	leftGoal.setNumChickens(chickens);
+	leftGoal.setNumWolves(wolves);
+	leftGoal.setBoat(bote);
+	//get contents from second line of goal.txt
+	goalFIN >> chickens >> temp69420 >> wolves >> temp >> bote;
+	//store them in rightGoal object
+	rightGoal.setNumChickens(chickens);
+	rightGoal.setNumWolves(wolves);
+	rightGoal.setBoat(bote);
+	//Our 4 objects now have all the data from the files so we are done
+
 }
