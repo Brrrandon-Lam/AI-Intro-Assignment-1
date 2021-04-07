77deab0bd56ddf73b882f4dc6601b60e1d3e316a
amills18 <millsae@oregonstate.edu>
2021-04-06 21:48:04 -0700

    Finished pseudocode and made sure our logic made sense. Additionally, we made some changes to the classes/objects

--- main.cpp
+++ main.cpp
@@ -15,27 +15,39 @@
 *  second line right: numChickens, numWolves, boat
 */
 
+class River {
+public:
+	int numObjects(); //return total number of objects on a river side
+	int getNumWolves(); // return num wolves
+	int getNumChickens(); //return num chickens
+	bool hasBoat(); //return 1 or 0 for boat being present
+	void setNumWolves(int wolfgang); //set num wolves.
+	void setNumChickens(int chickengang); //set num chickens
+	void setBoat(bool boatVal); //set boat to 1 or 0
+private:
+	int numWolves;
+	int numChickens;
+	bool boat;
+};
+
 class GameState {
 	public:
 		River leftBank;
 		River rightBank;
-	private:
+		//friend bool operator== (GameState& left, GameState& right);
 };
 
-class River: public GameState {
-	public:
-		int numObjects(); //return total number of objects on a river side
-		int getNumWolves(); // return num wolves
-		int getNumChickens(); //return num chickens
-		bool hasBoat(); //return 1 or 0 for boat being present
-		void setNumWolves(int wolfgang); //set num wolves.
-		void setNumChickens(int chickengang); //set num chickens
-		void setBoat(bool boatVal); //set boat to 1 or 0
-	private:
-		int numWolves;
-		int numChickens;
-		bool boat;
-};
+/*bool operator== (GameState& left, GameState& right) {
+	bool b;
+	//left.leftBank
+	//left.rightBank
+	//right.leftBank
+	//right.rightBank
+
+	//if all of those line up for left and right, then return true
+	return b;
+}*/
+
 
 struct Node { //keep track of the parent node, current node and depth.
 	GameState currentState;
@@ -91,7 +103,7 @@ bool River::hasBoat() {
 
 //Function Declarations
 
-void readFromFiles(River&, River&, River&, River&, std::ifstream&, std::ifstream&);
+void readFromFiles(GameState&, GameState&, std::ifstream&, std::ifstream&);
 
 int main(int argc, char** argv) {
 	if (argc != 5) { //check for correct number of arguments
@@ -174,7 +186,7 @@ int main(int argc, char** argv) {
 ******************************************************************************************************************/
 
 
-void readFromFiles(River& leftBank, River& rightBank, River& leftGoal, River& rightGoal, std::ifstream& initialFIN, std::ifstream& goalFIN) {
+void readFromFiles(GameState& state, GameState& goalState, std::ifstream& initialFIN, std::ifstream& goalFIN) {
 	int chickens = 0;
 	int wolves = 0;
 	bool bote;
@@ -183,9 +195,9 @@ void readFromFiles(River& leftBank, River& rightBank, River& leftGoal, River& ri
 	//get contents from first line of the initial.txt
 	initialFIN >> chickens >> temp69420 >> wolves >> temp >> bote;
 	//store them into the object (leftBank)
-	leftBank.setNumChickens(chickens);
-	leftBank.setNumWolves(wolves);
-	leftBank.setBoat(bote);
+	state.leftBank.setNumChickens(chickens);
+	state.leftBank.setNumWolves(wolves);
+	state.leftBank.setBoat(bote);
 
 	/*
 	std::cout << "Left Bank Initial " << chickens << " " << wolves << " " << bote << std::endl;
@@ -195,24 +207,24 @@ void readFromFiles(River& leftBank, River& rightBank, River& leftGoal, River& ri
 	//get contents from second line of initial.txt
 	initialFIN >> chickens >> temp >> wolves >> temp69420 >> bote;
 	//store in rightBank object
-	rightBank.setNumChickens(chickens);
-	rightBank.setNumWolves(wolves);
-	rightBank.setBoat(bote);
+	state.rightBank.setNumChickens(chickens);
+	state.rightBank.setNumWolves(wolves);
+	state.rightBank.setBoat(bote);
 	
 	//std::cout << "Right Bank Initial " << chickens << " " << wolves << " " << bote << std::endl;
 
 	//get contents from first line of goal.txt
 	goalFIN >> chickens >> temp >> wolves >> temp69420 >> bote;
 	//store them in leftGoal object
-	leftGoal.setNumChickens(chickens);
-	leftGoal.setNumWolves(wolves);
-	leftGoal.setBoat(bote);
+	goalState.leftBank.setNumChickens(chickens);
+	goalState.leftBank.setNumWolves(wolves);
+	goalState.leftBank.setBoat(bote);
 	//get contents from second line of goal.txt
 	goalFIN >> chickens >> temp69420 >> wolves >> temp >> bote;
 	//store them in rightGoal object
-	rightGoal.setNumChickens(chickens);
-	rightGoal.setNumWolves(wolves);
-	rightGoal.setBoat(bote);
+	goalState.rightBank.setNumChickens(chickens);
+	goalState.rightBank.setNumWolves(wolves);
+	goalState.rightBank.setBoat(bote);
 	//Our 4 objects now have all the data from the files so we are done
 
 }
@@ -232,20 +244,20 @@ void readFromFiles(River& leftBank, River& rightBank, River& leftGoal, River& ri
 *   Implement using a LIFO queue (Stack) <-This makes the generic graph search a depth first search
 */
 
-Node depthFirstSearch(GameState& state, GameState& goalState) {
+Node* depthFirstSearch(GameState& state, GameState& goalState) {
 	
 	int nodesExpanded = 0;
 	//Create our initial node that contains the initial game state.
-	Node initialState = new Node(); //create new node 
+	Node* initialState = new Node(); //create new node 
 	initialState->currentState = state; //store the passed game state (the initial state)
 	initialState->previous = NULL; //the initial game state has no previous game state, so set to null.
 
 	//initialize the explored set as empty
-	vector<Node> exploredSet;
+	std::vector<Node*> exploredSet; //push_back()
 	//create the path to hold nodes that we will expand on (the frontier)
-	stack<Node> frontier;
+	std::stack<Node*> frontier; //push()
 	//push the initial state onto the frontier
-	frontier.push_back(initialState);
+	frontier.push(initialState);
 
 	while (!frontier.empty()) {
 		//get the last-in value of the frontier.
@@ -253,27 +265,105 @@ Node depthFirstSearch(GameState& state, GameState& goalState) {
 		//remove the value from the frontier
 		frontier.pop();
 		//BASE CASE: If the initial state is the goal state, then we have already found the shortest path to the goal and can simply return the initial state.
+		
 		if (initialState->currentState == goalState) {
-			return initialState;
+			return initialState; //may need to operator overload the == for object comparison.
 		}
-		nodesExpanded++;
-		Node* successorNode;
 
-		frontier.push_back(successor(initialState, successorNode))
+		//NEED TO KEEP TRACK OF EXPANDED NODES (NUMBER) UPDATE BEFORE EXPANSION, BUT AFTER POPPING FROM STACK
+		//DONT EXPAND IF IT IS A GOAL NODE
+
+		//nodesExpanded++;
+
+
+		//expansion function()
+			//expands the popped node
+
+			//check and make sure the current node is not in the explored set
+				//for runtime purposes
+			//calls successor on the popped node
+				//determines which states are valid to go to from the current
+				//if successor finds one, place it in the frontier
+					//then place the popped node into the exploredSet (initialState Node)
+				
+
+
+
+		Node* successorNode;
+		
+
+		//frontier.push_back(successor(initialState, successorNode))
+
+		//all possible actions that can be done:
+
 		//if we want to move one chicken and one wolf
 		//if we want to move two chickens
 		//if we want to move two wolves
 		//if we want to move one chicken
 		//if we want to move one wolf
 
+		//if( we are at initial state )
+		// then we can move 1 chik and 1 wolf --> resulting state is a successor (AND hasn't already been added to the frontier or explored set)
+		// insert this state/node into the frontier for evaluation
+		// same with 1 wolf and 2 wolves
+		// same with 2 chickens and 1 chik
+		// all of the true possibilities should be added to the frontier, then pop the top of the stack
+		// continue until we find a successor state that is the goal state
+		// return path or unsuccessful
+
+		//if( successor state leads to an already visited state (exploredSet))
+			//essentially only add a state that has not been in the frontier OR the explored set
+			//only adds unique states
+		// then ignore that option
+		// do not include it into the frontier, as itis the graph version
+		//else
+		// add as indicated
+		return;
 	}
 
 	return NULL; //if we exit the while loop with nothing then there was no solution
 }
 
+/*******************************************************************************************************************
+* Function Name: successor
+* Description: returns possible states that are a result of an action and the state provided
+* Parameters: 2 node objects
+* Output: list of nodes to add to the frontier list
+******************************************************************************************************************/
+
 Node successor(Node& initialState, Node& successorNode) {
+	//should return the successors of a given node (successorNode) from the given state (initialState)
+	//the successors are the potential states that could result from one action being taken from the current state
+	//this list of successors should then be added to the frontier in the dfs
+
+
+	if (initialState.currentState.leftBank.hasBoat() == 0) { //boat is on the right bank
+		//CONDITIONS FOR MOVING:
+			//Must move one animal, up to 2, but as long as you are not leaving more wolves than chiks behind
+			//Keep track of num of overal animals transported over 
+			//if there is only one animal left on the bank, immediately transport it and follow the resulting action
+
+
+		int animalsRem = initialState.currentState.rightBank.numObjects() - 1; //animals left to be transported
+	}
+	else { //bote in left bank
+		//move boat over to the right bank for transportation of animals
+		initialState.currentState.leftBank.setBoat(0); //moves boat over from left bank
+		initialState.currentState.rightBank.setBoat(1); //boat now in right
+
+		//now move animals within the constraints
+		//preferably with a function, so that the if and else can just call it
+	}
 
 }
+
+
+/* Iterative Deepening DFS
+* DFS that searches based off the depth of the tree/graph
+* starts off with depth = 0, expands those nodes according to dfs
+* determines if the node is the goal node, if not then add 1 to the depth search
+* continue until a solution has been found
+*/
 
 
 /* Breadth First Search
@@ -290,3 +380,8 @@ Node successor(Node& initialState, Node& successorNode) {
 *
 *   Implement using a FIFO Queue <-This makes the generic graph search a Breadth First Search
 */
+
+
+//theoretically we should be able to use what we have written for the dfs for all the other uninformed search algos
+//just change the specs of the algorithm, FIFO, itteratively deepening LIFO, priority queue, etc.
+
